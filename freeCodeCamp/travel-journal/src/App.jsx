52e9f181d4e5abbd15d890f6b0628dd986279741
NGUYENVIETTIEN <69006525+viettienmilo@users.noreply.github.com>
2025-10-18 21:00:16 +0700

    finished Journal

--- freeCodeCamp/travel-journal/src/App.jsx
+++ freeCodeCamp/travel-journal/src/App.jsx
@@ -4,13 +4,22 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import Header from './components/Header.jsx';
 import Entry from './components/Entry.jsx';
+import journalData from './data.js';
 
 function App() {
-
   return (
     <>
       <Header />
-      <Entry />
+      <main className='container'>
+        {
+          journalData.map(journal =>
+            <Entry
+              key={journal.id}
+              {...journal}
+            />
+          )
+        }
+      </main>
     </>
   )
 }
