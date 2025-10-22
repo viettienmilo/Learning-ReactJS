4f37e287e45e445a259576cea3fb898fd760e7af
viettienmilo <viettien.milo@gmail.com>
2025-10-22 11:58:28 +0700

    Update Tenzies game

--- freeCodeCamp/tenzies/src/App.jsx
+++ freeCodeCamp/tenzies/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useRef, useEffect } from 'react'
 import './App.css'
 import Dice from './components/Dice.jsx';
 import { nanoid } from 'nanoid';
@@ -6,8 +6,14 @@ import ReactConfetti from 'react-confetti';
 
 function App() {
   const [faces, setFaces] = useState(() => genRandomFaces());
+  const [count, setCount] = useState(0);
+  const [time, setTime] = useState(0);
+  const intervalRef = useRef(null);
+  const newGameRef = useRef(null);
+
   const gameWon = faces.every(({ isHeld, value }) => (isHeld === true && value === faces[0].value));
 
+  // generate all 10 dices with random face
   function genRandomFaces() {
     let values = Array.from({ length: 10 }, () => Math.ceil(Math.random() * 6));
     return values.map(val => ({ id: nanoid(), value: val, isHeld: false }));
@@ -15,21 +21,46 @@ function App() {
 
   function rerollDice() {
     if (gameWon) {
-      setFaces(genRandomFaces())
+      setFaces(genRandomFaces()) // reset faces
+      setCount(0); // reset counter
+      setTime(0);  // reset timer
     }
     else {
+      // random faces that not in hold state
       setFaces(prevFaces => prevFaces.map(prevFace => {
         return prevFace.isHeld ? prevFace : { ...prevFace, value: Math.ceil(Math.random() * 6) }
       })
       )
+      setCount(prev => prev + 1); // increase counter
     }
   }
 
   function handleDiceClick(id) {
+    // invert hold state of dice with id
     setFaces(prevFaces =>
       prevFaces.map(prevFace => { return prevFace.id === id ? { ...prevFace, isHeld: !prevFace.isHeld } : prevFace })
     );
+  }
 
+  useEffect(() => {
+    if (gameWon) {
+      newGameRef.current.focus(); // set focus to button to play new game
+      clearInterval(intervalRef.current); // reset watchdog timer
+    }
+    else {
+      clearInterval(intervalRef.current); // reset watchdog timer
+      // start watchdog timer
+      intervalRef.current = setInterval(() => setTime(prev => prev + 10), 10);
+    }
+    return () => clearInterval(intervalRef.current); // clean function
+  }, [gameWon])
+
+  // display watchdog timer in format '00:00'
+  function formatTime(totalmillis) {
+    const totalSecs = Math.floor(totalmillis / 1000);
+    const mins = Math.floor(totalSecs / 60);
+    const secs = totalSecs % 60;
+    return `${mins.toString().padStart(2, '0')}:${secs.toString().padStart(2, '0')}`
   }
 
   return (
@@ -42,7 +73,11 @@ function App() {
       <div className='dice-container'>
         {faces.map(obj => <Dice dice={obj} key={obj.id} diceClick={handleDiceClick} />)}
       </div>
-      <button onClick={rerollDice}>{gameWon ? "New Game" : "Roll"}</button>
+      <div className="counter-timer-container">
+        <div>Counter: {count} </div>
+        <div ref={intervalRef}>Timer: {formatTime(time)}</div>
+      </div>
+      <button ref={newGameRef} onClick={rerollDice}>{gameWon ? "New Game" : "Roll"}</button>
     </main>
   )
 }
--- freeCodeCamp/tenzies/src/components/Dice.jsx
+++ freeCodeCamp/tenzies/src/components/Dice.jsx
@@ -1,9 +1,16 @@
-export default function Dice(props) {
+import './Dice.css';
+const Pip = () => <span className="pip" />;
+const Face = ({ children }) => <div className="face">{children}</div>;
 
+export default function Dice(props) {
     const { id, value, isHeld } = props.dice;
     const diceClick = props.diceClick;
 
+    let pips = Array(value).fill(0).map((_, i) => <Pip key={i} />)
+
     return (
-        <button className={isHeld ? "held-dice" : ""} onClick={() => diceClick(id)}>{value}</button>
+        <button className={isHeld ? "held-dice" : ""} onClick={() => diceClick(id)}>
+            <Face>{pips}</Face>
+        </button> //"held-dice" 
     );
 }
